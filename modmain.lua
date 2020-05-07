b0591ecaf33e0b2f700f8082df1e42bcff8a49e5
Ricky <rjromanach@gmail.com>
2020-05-07 05:12:27 -0500

    Added comments for potential viewers

--- modmain.lua
+++ modmain.lua
@@ -4,6 +4,7 @@ local TEMPLATES = GLOBAL.require ("widgets/redux/templates")
 --local io = GLOBAL.require("io")
 local KEY = GetModConfigData("KEY")
 
+--later used when recreating the custom modstab widget
 local settings = {
 	is_configuring_server = false,
 	details_width = 505,
@@ -14,25 +15,28 @@ local settings = {
 ---  Key Handler
 ---===============
 local modkey = KEY
+--The key will activate the modsscreen screen in-game
 GLOBAL.TheInput:AddKeyUpHandler(
 	modkey:lower():byte(), 
 	function()
 		if not GLOBAL.IsPaused() and IsDefaultScreen() then
 			local ms = ModsScreen()
-			ms.mods_page:Kill()
+			ms.mods_page:Kill() --Kill the modstab so we can customize it later
 			for k,v in pairs(ms.mods_page.subscreener.menu.items) do
-				v:Kill()
+				v:Kill() --Kills duplicated buttons that appear when the modstab is created again later
 			end
+			
 			ms.mods_page.tooltip:Kill()
 			ms.mods_page = ms.optionspanel:InsertWidget(ModsTab(ms, settings))
-			ms.mods_page.slotnum = GLOBAL.SaveGameIndex.current_slot
-			ms.bg:Kill()
-			ms.bg = ms.root:AddChild(TEMPLATES.BackgroundTint(0.5, {0,0,0}))
+			ms.mods_page.slotnum = GLOBAL.SaveGameIndex.current_slot --This satisfies a condition in ModsTab:Apply() that ensures the Sim does not reset
+			ms.bg:Kill() --Kill the default background
+			ms.bg = ms.root:AddChild(TEMPLATES.BackgroundTint(0.5, {0,0,0})) --Add custom background
 			ms.bg:MoveToBack()
 			
-			GLOBAL.TheFrontEnd:PushScreen(ms)
+			GLOBAL.TheFrontEnd:PushScreen(ms) --Finally make the screen appear
+			
+			--Extra code used for testing
 		--[[
-
 			local command = ""
 			for line in io.lines("../mods/IngameModManager/command.txt") do
 				command = command.." "..line
@@ -50,13 +54,14 @@ function IsDefaultScreen()
 	end
 end
 
+--What happens when the Apply button is clicked
 local function ApplyToGame(modsscreen)
 	local OldApply = modsscreen.Apply
 	modsscreen.Apply = function(self)
 		if GLOBAL.ThePlayer ~= nil then
 			GLOBAL.c_save()
 			OldApply(self)
-			GLOBAL.TheWorld:DoTaskInTime(2.5, function() GLOBAL.c_reset() end)
+			GLOBAL.TheWorld:DoTaskInTime(2.5, function() GLOBAL.c_reset() end) --give time for c_save and OldApply to work
 		end
 	end
 end
