5ba293903bc14c52fd2d5b620668038900b3b530
Alberto Romañach <albertoromanach@gmail.com>
2020-05-07 01:53:45 -0400

    Main functionality of mods screen
    The mods screen shows up just like it does when you view them from the main menu. In game, when you click apply, it will save and reset the game instead of take you back to the main menu screen.

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
 name = "In-game Mod Management"
-description = ""
+description = "Access your mods from in-game. You can enable, disable, and configure your mods without exiting the game now.\nThe default key is set to \' next to the enter key"
 author = "rawii22 & lord_of_les_ralph"
 version = "1.0"
 icon = "modicon.tex"
@@ -14,7 +14,46 @@ dst_compatible = true
 all_clients_require_mod = true
 client_only_mod = false
 
-configuration_options =
-{
-
+configuration_options = {
+  {
+    name = "KEY",
+    label = "Mod Screen Shortcut",
+    default = "\'",
+    options = {
+		{description = "A", data = "A"},
+		{description = "B", data = "B"},
+		{description = "C", data = "C"},
+		{description = "D", data = "D"},
+		{description = "E", data = "E"},
+		{description = "F", data = "F"},
+		{description = "G", data = "G"},
+		{description = "H", data = "H"},
+		{description = "I", data = "I"},
+		{description = "J", data = "J"},
+		{description = "K", data = "K"},
+		{description = "L", data = "L"},
+		{description = "M", data = "M"},
+		{description = "N", data = "N"},
+		{description = "O", data = "O"},
+		{description = "P", data = "P"},
+		{description = "Q", data = "Q"},
+		{description = "R", data = "R"},
+		{description = "S", data = "S"},
+		{description = "T", data = "T"},
+		{description = "U", data = "U"},
+		{description = "V", data = "V"},
+		{description = "W", data = "W"},
+		{description = "X", data = "X"},
+		{description = "Y", data = "Y"},
+		{description = "Z", data = "Z"},
+		{description = "[", data = "["},
+		{description = "]", data = "]"},
+		{description = "\\", data ="\\"},
+		{description = ";", data = ";"},
+		{description = "\'", data ="\'"},
+		{description = "/", data = "/"},
+		{description = "-", data = "-"},
+		{description = "=", data = "="}
+	}
+  },
 }
--- modmain.lua
+++ modmain.lua
@@ -1,15 +1,43 @@
-local io = GLOBAL.require("io")
+local ModsScreen = GLOBAL.require("screens/redux/modsscreen") 
+local ModsTab = GLOBAL.require("widgets/redux/modstab")
+local TEMPLATES = GLOBAL.require ("widgets/redux/templates")
+--local io = GLOBAL.require("io")
+local KEY = GetModConfigData("KEY")
 
-local modkey = "\'"
+local settings = {
+	is_configuring_server = false,
+	details_width = 505,
+	are_servermods_readonly = false,
+}
+
+---===============
+---  Key Handler
+---===============
+local modkey = KEY
 GLOBAL.TheInput:AddKeyUpHandler(
 	modkey:lower():byte(), 
 	function()
 		if not GLOBAL.IsPaused() and IsDefaultScreen() then
+			local ms = ModsScreen()
+			ms.mods_page:Kill()
+			for k,v in pairs(ms.mods_page.subscreener.menu.items) do
+				v:Kill()
+			end
+			ms.mods_page.tooltip:Kill()
+			ms.mods_page = ms.optionspanel:InsertWidget(ModsTab(ms, settings))
+			ms.mods_page.slotnum = GLOBAL.SaveGameIndex.current_slot
+			ms.bg:Kill()
+			ms.bg = ms.root:AddChild(TEMPLATES.BackgroundTint(0.5, {0,0,0}))
+			ms.bg:MoveToBack()
+			
+			GLOBAL.TheFrontEnd:PushScreen(ms)
+		--[[
+
 			local command = ""
 			for line in io.lines("../mods/IngameModManager/command.txt") do
 				command = command.." "..line
 			end
-			GLOBAL.ExecuteConsoleCommand(command)
+			GLOBAL.ExecuteConsoleCommand(command)]]
 		end
 	end
 )
@@ -21,3 +49,15 @@ function IsDefaultScreen()
 		return false
 	end
 end
+
+local function ApplyToGame(modsscreen)
+	local OldApply = modsscreen.Apply
+	modsscreen.Apply = function(self)
+		if GLOBAL.ThePlayer ~= nil then
+			GLOBAL.c_save()
+			OldApply(self)
+			GLOBAL.TheWorld:DoTaskInTime(2.5, function() GLOBAL.c_reset() end)
+		end
+	end
+end
+AddClassPostConstruct("screens/redux/modsscreen", ApplyToGame)
